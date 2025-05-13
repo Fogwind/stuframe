aa09aea4628079cc842d83411e3e2475279469ba
zgl <2505036090@qq.com>
2025-05-13 10:53:47 +0800

    第四章 路由配置

--- web/front.php
+++ web/front.php
@@ -10,26 +10,26 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
+$routes = include __DIR__.'/../src/app.php';
 
+$context = new Routing\RequestContext();
+$context->fromRequest($request);
+$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-$map = [
-    '/hello' => 'hello',
-    '/bye'   => 'bye',
-];
-
-$path = $request->getPathInfo();
-
-if(isset($map[$path])) {
-    ob_start();
+try {
     // https://www.php.net/manual/zh/function.extract.php
-    extract($request->query->all(), EXTR_SKIP);
-    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
+    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
     $response = new Response(ob_get_clean());
-} else {
-    $response = new Response('Not Found',404);
+} catch (Routing\Exception\ResourceNotFoundException $exception) {
+    $response = new Response('Not Found', 404);
+} catch (Exception $exception) {
+    $response = new Response('An error occurred', 500);
 }
 
 $response->send();
